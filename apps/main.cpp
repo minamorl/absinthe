ab4ba6da8a1b19632a28fe85bb6a40e3d4fde949
minamorl <minamorl@users.noreply.github.com>
2020-10-20 07:56:21 +0900

    Add scale generator

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,7 +1,23 @@
+#include <type_traits>
 #include <iostream>
 
-#include "lib.h"
+#include "chord.h"
 
 int main() {
-    std::cout << greeting::greeting() << std::endl;
+    using namespace absinthe;
+    static_assert(std::is_same_v<absinthe::move_tone_forward<absinthe::tone<3>, 5>::type, absinthe::tone<8>>);
+    /*
+    static_assert(std::is_same_v<
+        decltype(absinthe::make_scale_on_tone<absinthe::tone<0>, absinthe::major_scale>{}())
+        , std::tuple<C, D, E, F, G, A, B, C>>);
+    */
+    
+    // static_assert(std::is_same_v<
+    //     absinthe::intervals_sum<std::tuple<>, interval<2>, interval<2>, interval<1>, interval<2>, interval<2>, interval<2>, interval<1>>::type,
+    //     std::tuple<interval<2>, interval<4>, interval<5>, interval<7>, interval<9>, interval<11>, interval<12>>
+    // >);
+ //   std::apply([](auto... ts){ ((std::cout << ts << "\n"), ...) << std::endl;}, intervals_sum<0, std::tuple<interval<2>>, interval<2>, interval<2>, interval<1>, interval<2>, interval<2>, interval<2>, interval<1>>::type{});
+
+    std::apply([](auto... ts){ ((std::cout << ts << "\n"), ...) << std::endl;}, absinthe::make_scale_on_tone<absinthe::tone<3>, absinthe::major_scale>{}());
+
 }
---
+++ include/chord.h
@@ -0,0 +1,94 @@
+#include <vector>
+#include <string>
+#include <tuple>
+#include <boost/hana.hpp>
+
+namespace absinthe {
+
+template <class... Ts>
+class progression {};
+
+class natural {};
+class sharp {};
+class flat {};
+
+template <int Height>
+class tone : public std::integral_constant<int, Height>
+{};
+
+
+using C = tone<0>;
+using Csharp = tone<1>;
+using D = tone<2>;
+using Dsharp = tone<3>;
+using E = tone<4>;
+using F = tone<5>;
+using Fsharp = tone<6>;
+using G = tone<7>;
+using Gsharp = tone<8>;
+using A = tone<9>;
+using Asharp = tone<10>;
+using B = tone<11>;
+
+template <class Tone, unsigned N>
+class move_tone_forward
+{
+public:
+    using type = tone<(Tone::value + N) % 12>;
+};
+
+template<int N>
+class interval : public std::integral_constant<int, N> {};
+
+template <class... Intervals>
+class abstract_scale {
+public:
+    static constexpr long long unsigned size = sizeof...(Intervals);
+    using intervals = std::tuple<Intervals...>;
+};
+
+template <size_t Index, class Container, class Head, class... Tail>
+class intervals_sum;
+
+template <size_t Index, class Head, class Tail1, class... Tails, class... ContainerType>
+class intervals_sum<Index, std::tuple<ContainerType...>, Head, Tail1, Tails...> {
+public:
+    using type = intervals_sum<Index + 1, std::tuple<ContainerType..., interval<std::tuple_element_t<Index, std::tuple<ContainerType...>>::value + Tail1::value>>, Tail1, Tails...>::type;
+};
+
+template <size_t Index, class Head, class Tail,  class... ContainerType>
+class intervals_sum<Index, std::tuple<ContainerType...>, Head, Tail> {
+public:
+    using type = std::tuple<ContainerType..., interval<std::tuple_element_t<Index, std::tuple<ContainerType...>>::value + Tail::value>>;
+};
+
+
+template <class Tone, class AbstractScale>
+class make_scale_on_tone;
+
+template <class Tone, class Interval, class... Intervals>
+class make_scale_on_tone<Tone, abstract_scale<Interval, Intervals...>>
+{
+public:
+    auto operator()() {
+        auto summed_scale_intervals = typename intervals_sum<0, std::tuple<Interval>, Interval, Intervals...>::type{};
+        return std::apply([&](auto... ts){ return std::make_tuple(Tone::value, typename move_tone_forward<Tone, decltype(ts)::value>::type{}...); }, summed_scale_intervals);
+    }
+};
+
+using major_scale = abstract_scale<interval<2>, interval<2>, interval<1>, interval<2>, interval<2>, interval<2>, interval<1>>;
+
+
+
+template <class... Intervals>
+class chord {
+public:
+    static constexpr long long unsigned size = sizeof...(Intervals);
+    using intervals = std::tuple<Intervals...>;
+};
+
+using major_triad = chord<interval<0>, interval<4>, interval<7>>;
+using minor_triad = chord<interval<0>, interval<3>, interval<7>>;
+
+
+} // absinthe
